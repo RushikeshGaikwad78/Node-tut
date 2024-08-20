6210b1c0d7c802cf618db1b4a470b017ee131ccf
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2024-08-20 19:21:49 +0530

    URL method in req object

--- 3httpmodule.js
+++ 3httpmodule.js
@@ -3,10 +3,13 @@ var http = require("http");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Hello world!!"); //writes response to the client
+    res.write(req.url); 
     res.end(); //ends the response
   })
   .listen(8080); //the server object listens on port 8080
+
+//req arguement consists of request from client as an object,
+//this object has property called url which holds the part of the url that comes after the domain name
 
 //The first argument of the res.writeHead() method is the status code, 200 means that all is OK, the second argument is an object containing the response headers.
 
