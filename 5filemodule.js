5fcec35c81a9607b79384e7f2340abca2bfc5521
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2024-08-20 23:55:14 +0530

    write file

--- 5filemodule.js
+++ 5filemodule.js
@@ -26,7 +26,14 @@ var fs = require("fs");
 
 //fs.open() : takes a "flag" as the second argument, if the flag is "w" for "writing", 
 //the specified file is opened for writing. If the file does not exist, an empty file is created
-fs.open('mynewfile2.txt','w',function(err,file){
+// fs.open('mynewfile2.txt','w',function(err,file){
+//     if(err) throw err;
+//     console.log('saved!!');
+// })
+
+//fs.writeFile() :  replaces the specified file and content if it exists. 
+//If the file does not exist, a new file, containing the specified content, will be created
+fs.writeFile('mynewfile3.txt','Hello content!!!' , function(err){
     if(err) throw err;
-    console.log('saved!!');
+    console.log('Saved!!!');
 })
