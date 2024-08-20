a64d1bcf0740a62c9dde8034a3b21670fdac19a4
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2024-08-20 23:39:05 +0530

    appendfile

--- 5filemodule.js
+++ 5filemodule.js
@@ -2,12 +2,24 @@ var http = require("http");
 var fs = require("fs");
 
 //reading from file
-http
-  .createServer(function (req, res) {
-    fs.readFile("demofile1.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(8080);
+// http
+//   .createServer(function (req, res) {
+//     fs.readFile("demofile1.html", function (err, data) {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       return res.end();
+//     });
+//   })
+//   .listen(8080);
+
+
+//creation of files :-
+//fs.appendFile()
+//fs.open()
+//fs.writeFile()
+
+//fs.appendFile() : appends specified content to a file. If the file does not exist, the file will be created
+fs.appendFile('mynewfile1.txt','hello content!!' ,function(err){
+    if(err) throw err;
+    console.log('saved!!');
+})
